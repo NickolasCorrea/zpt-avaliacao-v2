93958f761ba5ddc6fd6ad424a5d966b86411a234
zpt <zpt@zpt.com.br>
2024-04-05 15:46:58 -0300

    fix(question10): missing arg in the Company constructor

--- src/Company.php
+++ src/Company.php
@@ -4,6 +4,10 @@ namespace Company;
 class Company {
 	private int $id;
 
+	public function __construct($id) {
+		$this->id = $id;
+	}
+
 	public function greetings() {
 		return "Greetings. Your ID is $this->id";
 	}
